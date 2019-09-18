9773ade022541e2cb8d1018268cc243990d61f39
guozhidi <15093738256@163.com>
2019-09-18 21:40:22 +0800

    nwe

---
+++ vue_jiekoulunbo.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="heads">
+			<van-swipe :autoplay="3000" indicator-color="white">
+			  <van-swipe-item v-for="img in data">
+			  	<img class="tu" :src="img.imageUrl"/>
+			  </van-swipe-item>
+			</van-swipe>	
+	</div>
+</template>
+
+<script>
+	import axios from 'axios';
+	export default{
+		name:'Home',
+		data(){
+			return{
+				data:[]
+			}
+		},
+		mounted(){
+			axios.get('http://net-music.penkuoer.com/banner').then(data=>{
+				this.data=data.data.banners
+			})
+			
+		},
+	}
+</script>
+
+<style scoped="">
+	.tu{
+		width:100vw;
+		height:200px;
+	}
+</style>
